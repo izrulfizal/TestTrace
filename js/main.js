bce52289f5b3285d68e57c237e0bd347e8cbd38e
izrulfizal <izrulfizal1410@gmail.com>
2024-03-30 22:51:07 +0800

    Update output display

--- js/main.js
+++ js/main.js
@@ -3,28 +3,40 @@ var testData = {
     timeTaken: 0,
     focusOff: 0,
     focusOn: 0,
+    keyboardStroke: 0,
+    cpdetect: false,
     
 }
 
 function displayData(){
     testData.answer = document.getElementById("q1-ans").value
+    if (testData.keyboardStroke == document.getElementById("q1-ans").value.length){
+        testData.cpdetect = false;
+    }
+    else{
+        testData.cpdetect = true;
+    }
     console.log(testData)
     testData.focusOff--
 
     if (testData.focusOff > 1){
         document.getElementById("card-body").innerHTML = `<p class="card-text">
-        Answer: ` + testData.answer + `<br>Student went off screen ` + testData.focusOff +  
-        ` times</p>`
+        Answer: ` + testData.answer + `<br>Student went off-screen <span class="offcounter">` + testData.focusOff +  
+        `</span> times</p>`
     }
     else if (testData.focusOff < 2){
         document.getElementById("card-body").innerHTML = `<p class="card-text">
-        Answer: ` + testData.answer + `<br>Student went off screen ` + testData.focusOff +  
-        ` time</p>`
+        Answer: ` + testData.answer + `<br>Student went off-screen <span class="offcounter">` + testData.focusOff +  
+        `</span> time</p>`
     }
     else{
         document.getElementById("card-body").innerHTML = `<p class="card-text">
-        Answer: ` + testData.answer + `<br>Student did not go off screen` + 
+        Answer: ` + testData.answer + `<br>Student did not go off-screen` + 
         `</p>`
+    }
+
+    if(testData.cpdetect == true){
+        document.getElementById("card-body").innerHTML =  document.getElementById("card-body").innerHTML + `<p class="cpdetect">Copy pasting detected!</p>`
     }
 
 
@@ -40,5 +52,5 @@ function stopFocus(){
 }
 
 function startWrite(){
-    console.log("Start writing")
+    testData.keyboardStroke++;
 }
