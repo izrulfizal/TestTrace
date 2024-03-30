e158b5444da02d045baa13f092aed17a3bbb8cfd
izrulfizal <izrulfizal1410@gmail.com>
2024-03-30 21:28:45 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var testData = {
+    answer: "",
+    timeTaken: 0,
+    focusOff: 0,
+    focusOn: 0,
+    
+}
+
+function displayData(){
+    testData.answer = document.getElementById("q1-ans").value
+    console.log(testData)
+    testData.focusOff--
+
+    document.getElementById("card-body").innerHTML = `<p class="card-text">
+    Answer: ` + testData.answer + `<br>Off focus: ` + testData.focusOff +  
+    `</p>`
+
+}
+
+function startFocus(){
+    testData.focusOn++;
+}
+
+function stopFocus(){
+    testData.focusOff++;
+    
+}
+
+function startWrite(){
+    console.log("Start writing")
+}
